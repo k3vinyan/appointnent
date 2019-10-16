a2a22bd646b3d65c47e6c688a6b0ab2942873d79
Kevin Yan <k3vinyan@gmail.com>
2019-10-15 22:59:28 -0700

    create searchbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SearchBar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from './components/Search.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    SearchBar
   }
 }
 </script>
---
+++ src/components/Search.vue
@@ -0,0 +1,60 @@
+<template>
+  <table>
+      <tr>
+          <th>Service</th>
+          <th>Stylist</th>
+          <th>Date</th>
+      </tr>
+      <tr>
+          <td>
+              <select>
+                    <option  
+                        v-for="service in services" 
+                        :value="service.item"
+                    >
+                    {{service}}
+                    </option>
+               </select>
+            </td>
+            <td>
+                <select>
+                    <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                    <option value="mercedes">Mercedes</option>
+                    <option value="audi">Audi</option>
+                </select>
+            </td>
+            <td>
+                <select>
+                    <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                    <option value="mercedes">Mercedes</option>
+                    <option value="audi">Audi</option>
+                </select>
+            </td>
+        </tr>
+  </table>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+    data: function() {
+            return {
+                services: [ "a", "b", "c"
+                    // {item: 'Directors Cut'},
+                    // {item: 'The Buzz'},
+                    // {item: 'Beard Trim'},
+                    // {item: 'Kids'}
+                ]
+            }
+            
+    }
+}
+</script>
+
+<style>
+    table, th, td {
+        border: 1px solid black;
+    }
+</style>
